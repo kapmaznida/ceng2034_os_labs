d8743e96dbb8edf6de0b797a0fc8e320510ca48c
kapmaznida <32363832+kapmaznida@users.noreply.github.com>
2019-03-16 23:48:52 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -3,25 +3,25 @@ import math
 
 print(os.getcwd())
 nida = os.getenv("HOME")
-os.chdir(home)                                          #change directory to user's home
+os.chdir(nida)                                          #change directory to user's home
 
 
 
-os.mkdir('lib64')                                       #create lib64 directory
-os.chdir('jail')                                        #change directory to jail
+os.mkdir('sec')                                         #create sec directory
+os.chdir('sec')                                        #change directory to sec
 os.mkdir('bin')                                         #create bin directory
-os.mkdir('jail')                                        #creating jail directory.
+os.mkdir('lib64')                                        #creating lib64 directory.
 os.mkdir ('lib')                                        #create lib directory
 os.mkdir ('etc')                                        #create etc directory
 
 
 
-os.system("cp -ru $(which bash) ~/jail/bin/")	        #copying the dependencies of bash
-os.system("cp -ru /lib/x86_64-linux-gnu  ~/jail/lib/ ")
-os.system("cp -ru /lib64/ ~/jail/")
+os.system("cp -ru $(which bash) ~/sec/bin/")	        #copying the dependencies of bash
+os.system("cp -ru /lib/x86_64-linux-gnu  ~/sec/lib/ ")
+os.system("cp -ru /lib64/ ~/sec/")
 
-os.chroot(home + "/jail")
+os.chroot(nida + "/sec")
 print(os.getcwd())
-print(math.sqrt(16))
+print(nidanınjail(16))
 print(os.getcwd())
 
